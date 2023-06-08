4b0b771fba7c87590c302300a7999687e94c0f64
20120206 <nguyenngocthuy9a1@gmail.com>
2023-06-09 02:42:16 +0700

    done UI for bill

--- Nemo/MainWindow.xaml.cs
+++ Nemo/MainWindow.xaml.cs
@@ -25,13 +25,75 @@ namespace Nemo
         public MainWindow()
         {
             InitializeComponent();
+            List<Bill> items = new List<Bill>();
+
+            items.Add(new Bill() { STT = 1, MaHD = 4, NgayTra = "01/05/2023", SoPhong = 1, KhachHang = "Nguyễn An", TongTien = 1100000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt"});
+            items.Add(new Bill() { STT = 2, MaHD = 3, NgayTra = "01/04/2023", SoPhong = 3, KhachHang = "Minatozaki Sana", TongTien = 2800000, DiaChi = "Sóc Trăng", PhuongThuc = "Chuyển khoản" });
+            items.Add(new Bill() { STT = 3, MaHD = 2, NgayTra = "01/03/2023", SoPhong = 2, KhachHang = "Sherlock Holmes", TongTien = 1550000, DiaChi = "Bình Dương", PhuongThuc = "Chuyển khoản" });
+            items.Add(new Bill() { STT = 4, MaHD = 1, NgayTra = "01/02/2023", SoPhong = 1, KhachHang = "Trần Thanh Trúc", TongTien = 670000 });
+            items.Add(new Bill() { STT = 1, MaHD = 4, NgayTra = "01/05/2023", SoPhong = 1, KhachHang = "Nguyễn An", TongTien = 1100000 });
+            items.Add(new Bill() { STT = 2, MaHD = 3, NgayTra = "01/04/2023", SoPhong = 3, KhachHang = "Minatozaki Sana", TongTien = 2800000 });
+            items.Add(new Bill() { STT = 3, MaHD = 2, NgayTra = "01/03/2023", SoPhong = 2, KhachHang = "Sherlock Holmes", TongTien = 1550000 });
+            items.Add(new Bill() { STT = 4, MaHD = 1, NgayTra = "01/02/2023", SoPhong = 1, KhachHang = "Trần Thanh Trúc", TongTien = 670000 });
+            items.Add(new Bill() { STT = 1, MaHD = 4, NgayTra = "01/05/2023", SoPhong = 1, KhachHang = "Nguyễn An", TongTien = 1100000 });
+            items.Add(new Bill() { STT = 2, MaHD = 3, NgayTra = "01/04/2023", SoPhong = 3, KhachHang = "Minatozaki Sana", TongTien = 2800000 });
+            items.Add(new Bill() { STT = 3, MaHD = 2, NgayTra = "01/03/2023", SoPhong = 2, KhachHang = "Sherlock Holmes", TongTien = 1550000 });
+            items.Add(new Bill() { STT = 4, MaHD = 1, NgayTra = "01/02/2023", SoPhong = 1, KhachHang = "Trần Thanh Trúc", TongTien = 670000 });
+            items.Add(new Bill() { STT = 1, MaHD = 4, NgayTra = "01/05/2023", SoPhong = 1, KhachHang = "Nguyễn An", TongTien = 1100000 });
+            items.Add(new Bill() { STT = 2, MaHD = 3, NgayTra = "01/04/2023", SoPhong = 3, KhachHang = "Minatozaki Sana", TongTien = 2800000 });
+            items.Add(new Bill() { STT = 3, MaHD = 2, NgayTra = "01/03/2023", SoPhong = 2, KhachHang = "Sherlock Holmes", TongTien = 1550000 });
+            items.Add(new Bill() { STT = 4, MaHD = 1, NgayTra = "01/02/2023", SoPhong = 1, KhachHang = "Trần Thanh Trúc", TongTien = 670000 });
+            items.Add(new Bill() { STT = 1, MaHD = 4, NgayTra = "01/05/2023", SoPhong = 1, KhachHang = "Nguyễn An", TongTien = 1100000 });
+            items.Add(new Bill() { STT = 2, MaHD = 3, NgayTra = "01/04/2023", SoPhong = 3, KhachHang = "Minatozaki Sana", TongTien = 2800000 });
+            items.Add(new Bill() { STT = 3, MaHD = 2, NgayTra = "01/03/2023", SoPhong = 2, KhachHang = "Sherlock Holmes", TongTien = 1550000 });
+            items.Add(new Bill() { STT = 4, MaHD = 1, NgayTra = "01/02/2023", SoPhong = 1, KhachHang = "Trần Thanh Trúc", TongTien = 670000 });
+            items.Add(new Bill() { STT = 1, MaHD = 4, NgayTra = "01/05/2023", SoPhong = 1, KhachHang = "Nguyễn An", TongTien = 1100000 });
+            items.Add(new Bill() { STT = 2, MaHD = 3, NgayTra = "01/04/2023", SoPhong = 3, KhachHang = "Minatozaki Sana", TongTien = 2800000 });
+            items.Add(new Bill() { STT = 3, MaHD = 2, NgayTra = "01/03/2023", SoPhong = 2, KhachHang = "Sherlock Holmes", TongTien = 1550000 });
+            items.Add(new Bill() { STT = 4, MaHD = 1, NgayTra = "01/02/2023", SoPhong = 1, KhachHang = "Trần Thanh Trúc", TongTien = 670000 });
+
+            //items.Add(new Bill() { STT = 1, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt"});
+            //items.Add(new Bill() { STT = 2, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 3, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 4, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 5, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 6, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 7, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 8, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 9, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 10, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 11, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 12, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 13, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 14, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 15, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+            //items.Add(new Bill() { STT = 16, MaPTP = 4, MaPhong = "A203", SoNgayThue = 2, SoKhach = 2, DonGia = 400000, TongTien = 800000, DiaChi = "TPHCM", PhuongThuc = "Tiền mặt" });
+
+            lvBill.ItemsSource = items;
+        }
+
+        private class Bill
+        {
+            public int STT { get; set; }
+            public int MaPTP { get; set; }
+            public string MaPhong { get; set; }
+            public int SoNgayThue { get; set; }
+            public int SoKhach { get; set; }
+            public long DonGia { get; set; }
+            public int MaHD { get; set; }
+            public string NgayTra { get; set; }
+            public int SoPhong { get; set; }
+            public string KhachHang { get; set; }
+            public long TongTien { get; set; }
+            public string DiaChi { get; set; }
+            public string PhuongThuc { get; set; }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //var KhachHang = new KhachHang()
             //{
-               // MaKH = "001",
+               // KhachHang = "001",
                 //TenKH = "Nguyễn Văn A",
                 //DiaChi = "Hồ Chí Minh"
            // };
