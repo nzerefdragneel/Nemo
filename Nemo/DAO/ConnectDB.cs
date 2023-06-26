1a14e6b3d87c163f27734cb5b2114a1987701240
nzerefdragneel <nguyentranngocsuong94@gmail.com>
2023-06-26 10:30:38 +0700

    sửa cn

--- Nemo/DAO/ConnectDB.cs
+++ Nemo/DAO/ConnectDB.cs
@@ -21,7 +21,7 @@ namespace Nemo.DAO
         public ConnectDB()
         {
             // Khởi tạo chuỗi kết nối
-            connString = "Server=localhost;Port=5432;Username=postgres;Password='hjhj';Database=postgres";
+            connString = "Server=localhost;Port=5432;Username=postgres;Password='1';Database=DB";
 
             // Khởi tạo đối tượng kết nối
             conn = new NpgsqlConnection(connString);
